55fcc0d5229649d996b78cac6abf7acb887d5d7d
duanesb <duanebollig.school@gmail.com>
2025-02-07 19:45:22 -0400

    delete useless content2
    .

--- views/intro.py
+++ views/intro.py
@@ -2,7 +2,7 @@ import flet as ft
 from objects import SimulatorButton
 
 def Intro_View(router):
-    content2 = ft.Container(
+    content = ft.Container(
         content= ft.Column(
             controls=[
                 ft.Text("Simulators", size=55, weight="bold"),
@@ -28,36 +28,4 @@ def Intro_View(router):
         height=800,
         padding=ft.padding.only(top=20)
     )
-    content = ft.Container(
-        content=ft.Column(spacing=25,controls=[
-            ft.Row(controls=[ft.Text("Simulators", size=35, weight="bold")],alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row(alignment=ft.MainAxisAlignment.SPACE_AROUND,controls=[
-                ft.Column(spacing=30,controls=[
-                    ft.Container(on_click=lambda _: router.go('/ohm'), content=ft.Column(controls=[
-                        ft.Text("Ohm's Law", size=30, weight="bold"),
-                        ft.Image(src="Ohm-intro.png", width=200, height=200, fit="contain"),
-                ],alignment=ft.MainAxisAlignment.CENTER, 
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER)),
-                    ft.Container(on_click=lambda _: router.go('/capacitance'), content=ft.Column(controls=[
-                        ft.Text("Capacitance", size=30, weight="bold"),
-                        ft.Image(src="Capacitance-intro.png", width=200, height=200, fit="contain"),
-                    ], alignment=ft.MainAxisAlignment.CENTER, 
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER)),
-                ]),
-
-                ft.Column(spacing=30,controls=[
-                    ft.Container(on_click=lambda _: router.go('/magnetic'), content=ft.Column(controls=[
-                        ft.Text("Magnetic Field", size=30, weight="bold"),
-                        ft.Image(src="Magnetic-intro.png", width=200, height=200, fit="contain"),
-                    ], alignment=ft.MainAxisAlignment.CENTER, 
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER)),
-                    ft.Container(on_click=lambda _: router.go('/resistance'), content=ft.Column(controls=[
-                        ft.Text("Resistance", size=30, weight="bold"),
-                        ft.Image(src="Resistance-intro.png", width=200, height=200, fit="contain"),
-                    ], alignment=ft.MainAxisAlignment.CENTER, 
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER)),
-                ])
-            ])
-        ])
-    )
-    return content2
+    return content
