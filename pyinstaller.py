e78266e05e38a1d4e0da5766bbc7cf069ce199c4
LordX123 <sebastian@varillas.net>
2025-03-31 11:29:27 -0400

    Changed

--- pyinstaller.py
+++ pyinstaller.py
@@ -3,12 +3,13 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     'main.py',
     '--name=Physics Simulators',
-    '--windowed',
+    # '--windowed',
     '--noconsole',
-    '--onedir',
+    '--onefile',
     '--add-data=assets:assets',
     '--icon=App_Icon.png',
 ])
 
 
 #! Remember that to reload using the spec, you use: pyinstaller Physics-Simulators.spec
+
